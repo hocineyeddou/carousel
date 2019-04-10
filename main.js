f182cb84264185333a87750dd1f26a725803bfe4
hocine yeddou <hocineyeddou.hy@gmail.com>
2019-04-10 11:55:56 +0200

    3modification

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
-'use strict'  
+'use strict' 
+
+
+let listImages = document.getElementsByClassName("imgCarousel");
+let count = 0;
+let indexLastImage =  listImages.length - 1;
+
+function imgNext(){
+
+    if (count >= listImages.length - 1){
+        count=0;
+        listImages[indexLastImage].classList.add("displayNone"); 
+        listImages[0].classList.remove("displayNone");
+  }
+    else {
+
+        listImages[count].classList.add("displayNone");
+        listImages[count+1].classList.remove("displayNone");
+        count++;
+
+
+  }
+ 
+
+}
+
+function imgPrevious(){
+  
+if (count <= listImages.length)
+  count=4;
+  listImages[indexLastImage].classList.add("displayNone"); 
+  listImages[0].classList.remove("displayNone");
+  count--;
+
+}
+
+}
+
+
+
+
 
 
 
